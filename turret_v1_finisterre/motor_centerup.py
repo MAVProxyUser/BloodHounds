21de6ea143ce902a2d3dd8c3cea0fe93d4a2ac44
Your Name <you@example.com>
2022-11-29 05:39:47 +0000

    Update Dynamixel code

--- turret_v1_finisterre/motor_centerup.py
+++ turret_v1_finisterre/motor_centerup.py
@@ -1,104 +1,85 @@
 #!/usr/bin/env python
 
 import os
-
-from dynamixel_sdk import *                    # Uses Dynamixel SDK library
-
-# Control table address
-ADDR_MX_TORQUE_ENABLE      = 24               # Control table address is different in Dynamixel model
+from dynamixel_sdk import *
+ADDR_MX_TORQUE_ENABLE      = 24
 ADDR_MX_GOAL_POSITION      = 30
 ADDR_MX_PRESENT_POSITION   = 36
-
-# Protocol version
-PROTOCOL_VERSION            = 1.0               # See which protocol version is used in the Dynamixel
-
-# Default setting
-DXL_ID                      = 1                 # Dynamixel ID : 1
-DXL_ID2                     = 2                 # Dynamixel ID : 1
-BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
-DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
-
-TORQUE_ENABLE               = 1                 # Value for enabling the torque
-TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 0           # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 4095            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-X_DXL_MOVING_STATUS_THRESHOLD = 6                # Dynamixel moving status threshold
-Y_DXL_MOVING_STATUS_THRESHOLD = 6                # Dynamixel moving status threshold
-
-# Center:[ID:001] X PresPos:2021 [ID:002] Y PresPos:2863
-x_dxl_goal_position = 2021         # X Center Goal position
-y_dxl_goal_position = 2863         # Y Center Goal position
+PROTOCOL_VERSION            = 1.0
+DXL_ID                      = 1
+DXL_ID2                     = 2
+BAUDRATE                    = 57600
+DEVICENAME                  = '/dev/ttyUSB0'
+TORQUE_ENABLE               = 1
+TORQUE_DISABLE              = 0
+DXL_MINIMUM_POSITION_VALUE  = 0
+DXL_MAXIMUM_POSITION_VALUE  = 4095
+X_DXL_MOVING_STATUS_THRESHOLD = 6
+Y_DXL_MOVING_STATUS_THRESHOLD = 6
+x_dxl_goal_position = 2021
+y_dxl_goal_position = 2863
 
 portHandler = PortHandler(DEVICENAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
+portHandler.openPort()
+portHandler.setBaudRate(BAUDRATE)
 
-# Open port
-if portHandler.openPort():
-    print("Succeeded to open the port")
-else:
-    print("Failed to open the port")
-    print("Press any key to terminate...")
-    quit()
-
-
-# Set port baudrate
-if portHandler.setBaudRate(BAUDRATE):
-    print("Succeeded to change the baudrate")
-else:
-    print("Failed to change the baudrate")
-    print("Press any key to terminate...")
-    quit()
-
-# Enable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Dynamixel has been successfully connected")
-
-# X Write goal position
-dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, x_dxl_goal_position)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-# Y Write goal position
-dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID2, ADDR_MX_GOAL_POSITION, y_dxl_goal_position)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-while 1:
-    # Read present position
-    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
+def enable_torque(id):
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, id, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
         print("%s" % packetHandler.getRxPacketError(dxl_error))
-    dxl_present_position2, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID2, ADDR_MX_PRESENT_POSITION)
+    else:
+        print("Dynamixel has been successfully Torque has been enabled")
+
+def write_goal(id, pos):
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, id, ADDR_MX_GOAL_POSITION, pos)
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
         print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-    print("[ID:%03d] X GoalPos:%03d  PresPos:%03d" % (DXL_ID, x_dxl_goal_position, dxl_present_position))
-    print("[ID:%03d] Y GoalPos:%03d  PresPos:%03d" % (DXL_ID2, y_dxl_goal_position, dxl_present_position2))
+def pos_read(id):
+    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, id, ADDR_MX_PRESENT_POSITION)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-    xflag, yflag = 0, 0
-    if abs(x_dxl_goal_position - dxl_present_position) < X_DXL_MOVING_STATUS_THRESHOLD:
-        print("X Hit threshhold")
-        xflag = 1
-    if abs(y_dxl_goal_position - dxl_present_position2) < Y_DXL_MOVING_STATUS_THRESHOLD:
-        print("Y Hit threshhold")
-        yflag = 1
-    if xflag == 1 & yflag == 1:
-        print("Turret is centered up")
-        break
+    print("[ID:%03d] PresPos:%03d" % (id, dxl_present_position))
+    return dxl_present_position
 
-# Close port
-portHandler.closePort()
+def hit_pos(pos, goal_pos, thresh):
+    if abs(goal_pos - pos) < thresh:
+        print("Hit threshhold")
+        return True
+
+def close_port():
+    portHandler.closePort()
+
+def main():
+    enable_torque(DXL_ID)
+    enable_torque(DXL_ID2)
+
+    write_goal(DXL_ID, x_dxl_goal_position)
+    write_goal(DXL_ID2, y_dxl_goal_position)
+
+    while 1:
+        x = hit_pos( pos_read(DXL_ID), x_dxl_goal_position, X_DXL_MOVING_STATUS_THRESHOLD)
+        y = hit_pos( pos_read(DXL_ID2), y_dxl_goal_position, Y_DXL_MOVING_STATUS_THRESHOLD)
+
+        if ( x and y):
+            print("Turret is centered up")
+            break
+        else:
+            if x != True:
+                print("X Not at goal: " + str(x_dxl_goal_position) + "instead at: " + str(pos_read(DXL_ID) ))
+            if y != True:
+                print("Y Not at goal: " + str(y_dxl_goal_position) + "instead at: " + str(pos_read(DXL_ID2) ))
+
+    close_port()
+
+if __name__ == "__main__":
+    main()
+
