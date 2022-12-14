9793bab197f6c0c6bafd32ece0138c4364a4d007
Your Name <you@example.com>
2022-12-14 20:02:06 +0000

    ROS2 pub sub trigger node example

---
+++ turret_v1_finisterre/py_pubsub_trigger/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'py_pubsub_trigger'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pi',
+    maintainer_email='you@example.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'talker = py_pubsub_trigger.publisher_member_function:main',
+                'listener = py_pubsub_trigger.subscriber_member_function:main',
+        ],
+    },
+)
